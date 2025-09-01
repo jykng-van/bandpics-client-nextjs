2b1eed15025c0e641527d7a9557b5e5daba86ce2
jykng-van <jykng@shaw.ca>
2025-08-31 18:26:34 -0700

    location list controls

--- src/app/components/location_picker.tsx
+++ src/app/components/location_picker.tsx
@@ -2,12 +2,15 @@ import { useContext, useState, useEffect } from "react"
 import { ImageGroupsContext } from "@/app/events/event_form";
 import { APIProvider, Map as GMap, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
 import { GetLocations } from "@/app/lib/event_actions";
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 
 export const LocationPicker = ()=>{
     const imageGroups = useContext(ImageGroupsContext);
 
     const [foundCoords, setFoundCoords] = useState<GmapCoords[]>([]);
+    const [groupedCoords, setGroupedCoords] = useState<Map<string, {images:string[]}>>(new Map());
     const [locationResult, setLocationResult] = useState<Location | null>(null);
     const [selectedPoint, setSelectedPoint] = useState<number>(0);
 
@@ -21,7 +24,7 @@ export const LocationPicker = ()=>{
         const factor = Math.pow(10, DECIMALS);
         const roundCoord = (num:number)=> Math.round(num * factor) / factor;
 
-        const coords = new Map<string, number>();
+        const coords = new Map<string, {images:string[]}>();
 
         //get all coordinates from all images in each group
         imageGroups.forEach(group=>{ //each group
@@ -31,21 +34,30 @@ export const LocationPicker = ()=>{
                     const lng = roundCoord(img.data.coords.longitude);
                     const key = JSON.stringify([lng,lat]);
                     console.log(img.filename, key, coords.get(key) || 0);
-                    coords.set(key, (coords.get(key) || 0) + 1 ); // Add or update the entry in the new Map
+                    //coords.set(key, (coords.get(key) || 0) + 1 );
+
+                    // Add or update the entry in the new Map
+                    const coord_data = coords.get(key) || {images:[]};
+                    coord_data.images.push(`${group.name}/[SIZE]/${img.filename}`);
+                    if (!coords.has(key)){
+                        coords.set(key, coord_data);
+                    }
                 }
             });
         });
+        setGroupedCoords(coords);
         const sorted_coords = getSortedCoords(coords)
         setFoundCoords(sorted_coords);
         console.log('sorted_coords', sorted_coords);
+        console.log('grouped_coords', coords);
         findLocations(sorted_coords[0]);
     }, [imageGroups]);
 
-    const getSortedCoords = (coords:Map<string, number>)=>{
-        return Array.from(coords).sort(([_coord1, found1],[_coord2, found2])=>{
-            if (found1 < found2){
+    const getSortedCoords = (coords:Map<string, {images:string[]}>)=>{
+        return Array.from(coords).sort(([_coord1, data1],[_coord2, data2])=>{
+            if (data1.images.length < data2.images.length){
                 return 1;
-            }else if (found1 > found2){
+            }else if (data1.images.length > data2.images.length){
                 return -1;
             }else{
                 return 0;
@@ -68,16 +80,25 @@ export const LocationPicker = ()=>{
     return(
         <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
             <div>{foundCoords.length} possible coordinates found</div>
-            <section className="grid grid-cols-[1fr_15rem] w-[95vw] h-[90vh]">
-                <div>
+            <section className="grid grid-cols-[1fr_20rem] grid-rows-[4rem_1fr] w-[95vw] h-[90vh]">
+                <div className="row-span-full">
                 {foundCoords &&
                 <GMap defaultZoom={18} defaultCenter={foundCoords[0]} style={{width:'100%', height:'100%'}} mapId={'venue-location'}>
                     {foundCoords.map((coord, index)=>(
-                        <AdvancedMarker key={`maker${index}`} position={coord} clickable={true} onClick={()=>setSelectedPoint(index)}>
+                        <AdvancedMarker key={`maker${index}`} position={coord} zIndex={index==selectedPoint ? 2:1}
+                        clickable={true} onClick={()=>setSelectedPoint(index)}>
                             <Pin background={index==selectedPoint ? '#147631ff':'#a5a1a1ff'} glyphColor={'#000'} borderColor={'#000'}></Pin>
                         </AdvancedMarker>
                     ))}
                 </GMap>}
+                </div>
+                <div className="flex flex-row items-center justify-stretch">
+                    <button><ArrowLeftIcon /></button>
+                    <div className="flex-1 text-center">
+                        Coordinate {selectedPoint + 1} <br />
+                        <em></em>
+                    </div>
+                    <button><ArrowRightIcon /></button>
                 </div>
                 <div>
                     <h3>Places Found</h3>
